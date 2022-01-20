ed340b314ee6cfa25051c2f522ff4d02550ff421
Raline Silva <ralinesilva1@gmail.com>
2022-01-20 16:50:27 -0300

    iniciar projeto

---
+++ src/main/java/br/com/zup/ZupNotion/ZupNotionApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupNotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupNotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupNotionApplication.class, args);
+	}
+
+}
