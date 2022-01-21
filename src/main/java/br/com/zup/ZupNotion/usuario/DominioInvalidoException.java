f3ae224d0534ada247a05a135225528f09557679
Anna Karolyne <dev.akvc@gmail.com>
2022-01-21 12:31:24 -0300

    criar tratamento para excecao de dominio invalido

---
+++ src/main/java/br/com/zup/ZupNotion/usuario/DominioInvalidoException.java
@@ -0,0 +1,8 @@
+package br.com.zup.ZupNotion.usuario;
+
+public class DominioInvalidoException extends RuntimeException {
+    public DominioInvalidoException(String message) {
+        super(message);
+    }
+
+}
