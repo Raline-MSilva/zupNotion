3f1a08b30f997a1184bd86d24268df675c1616c7
Raline Silva <ralinesilva1@gmail.com>
2022-01-21 11:16:49 -0300

    criar metodo para verificar email existente

--- src/main/java/br/com/zup/ZupNotion/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/ZupNotion/usuario/UsuarioRepository.java
@@ -3,5 +3,6 @@ package br.com.zup.ZupNotion.usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository <Usuario, String> {
+    boolean existsByEmail(String email);
 
 }
