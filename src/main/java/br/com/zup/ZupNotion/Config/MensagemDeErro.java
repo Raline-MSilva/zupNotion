3dd522d8514cb2503a0708af07f6b40c342771c1
Raline Silva <ralinesilva1@gmail.com>
2022-01-21 11:17:31 -0300

    criar classe mensagem de erro

---
+++ src/main/java/br/com/zup/ZupNotion/Config/MensagemDeErro.java
@@ -0,0 +1,11 @@
+package br.com.zup.ZupNotion.Config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MensagemDeErro {
+    private String mensagem;
+
+}
