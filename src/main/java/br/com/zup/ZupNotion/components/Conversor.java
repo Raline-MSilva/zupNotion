bd721943c446becc920001428c9b3de003871011
Savio <pauloluizsavio@gmail.com>
2022-01-21 14:14:21 -0300

    criar conversor

---
+++ src/main/java/br/com/zup/ZupNotion/components/Conversor.java
@@ -0,0 +1,15 @@
+package br.com.zup.ZupNotion.components;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
