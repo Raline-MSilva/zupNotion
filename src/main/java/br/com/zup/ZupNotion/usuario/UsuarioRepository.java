6bf98b5159d56c1901c80f88da6d397c8b9cc3d5
Anna Karolyne <dev.akvc@gmail.com>
2022-01-24 12:07:47 -0300

    criar query findByEmail

--- src/main/java/br/com/zup/ZupNotion/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/ZupNotion/usuario/UsuarioRepository.java
@@ -2,7 +2,10 @@ package br.com.zup.ZupNotion.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository <Usuario, String> {
     boolean existsByEmail(String email);
 
+    Optional<Usuario> findByEmail(String email);
 }
