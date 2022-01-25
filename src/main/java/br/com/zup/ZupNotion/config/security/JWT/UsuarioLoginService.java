78bbfeba3f50f1f1cc8ac82c28c0ce137c59730d
Raline Silva <ralinesilva1@gmail.com>
2022-01-25 14:11:04 -0300

    modelar classe usuario logado service

---
+++ src/main/java/br/com/zup/ZupNotion/config/security/JWT/UsuarioLoginService.java
@@ -0,0 +1,28 @@
+package br.com.zup.ZupNotion.config.security.JWT;
+
+import br.com.zup.ZupNotion.models.Usuario;
+import br.com.zup.ZupNotion.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioLoginService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+
+        usuarioOptional.orElseThrow(() ->  new UsernameNotFoundException("Email ou Senha Invalido"));
+        Usuario usuario = usuarioOptional.get();
+
+        return new UsuarioLogado(usuario.getId(), usuario.getEmail(), usuario.getSenha());
+    }
+}
