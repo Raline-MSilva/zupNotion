ff6c4fe798a80a3d6504fdfb8aab3eaddcef2d37
Elaine-dss <elaine.silva@zup.com.br>
2022-01-25 15:57:35 -0300

    finalizar configuracoes de seguranca

--- src/main/java/br/com/zup/ZupNotion/config/security/ConfiguracoesDeSeguranca.java
+++ src/main/java/br/com/zup/ZupNotion/config/security/ConfiguracoesDeSeguranca.java
@@ -1,12 +1,19 @@
 package br.com.zup.ZupNotion.config.security;
 
+import br.com.zup.ZupNotion.config.security.JWT.FiltroDeAutenticacaoJWT;
+import br.com.zup.ZupNotion.config.security.JWT.FiltroDeAutorizacaoJWT;
+import br.com.zup.ZupNotion.config.security.JWT.JWTComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,6 +22,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JWTComponent jwtComponent;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +37,14 @@ public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/usuario").permitAll().anyRequest()
                 .authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+       http.addFilter(new FiltroDeAutenticacaoJWT(jwtComponent, authenticationManager()));
+       http.addFilter(new FiltroDeAutorizacaoJWT(authenticationManager(), jwtComponent, userDetailsService));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
@@ -41,6 +61,11 @@ public class ConfiguracoesDeSeguranca extends WebSecurityConfigurerAdapter {
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManager();
     }
 
 }
