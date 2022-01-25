a5f54ebc66f2cb7261b0bfa50048af6f92b0e0fe
Elaine-dss <elaine.silva@zup.com.br>
2022-01-25 15:05:27 -0300

    autenticar usuario com sucesso

--- src/main/java/br/com/zup/ZupNotion/config/security/JWT/FiltroDeAutenticacaoJWT.java
+++ src/main/java/br/com/zup/ZupNotion/config/security/JWT/FiltroDeAutenticacaoJWT.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -22,7 +24,8 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
     private AuthenticationManager authenticationManager;
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
@@ -37,6 +40,19 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
         } catch (IOException e) {
             throw new AcessoNegadoException();
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        UsuarioLogado usuarioLogado = (UsuarioLogado) authResult.getPrincipal();
+        String username = usuarioLogado.getUsername();
+        String id = usuarioLogado.getId();
+
+        String token = jwtComponent.gerarToken(username, id);
+
+        response.setHeader("Access-Control-Expose-Headers","Authorization");
+        response.addHeader("Authorization", "Token "+token);
     }
 
 }
