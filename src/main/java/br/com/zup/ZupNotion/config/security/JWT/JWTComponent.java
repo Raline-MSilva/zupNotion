7b9ae1f1f8133b3dbfc1312881d2ee53dbb62fb7
Raline Silva <ralinesilva1@gmail.com>
2022-01-25 14:27:31 -0300

    pegar claims

--- src/main/java/br/com/zup/ZupNotion/config/security/JWT/JWTComponent.java
+++ src/main/java/br/com/zup/ZupNotion/config/security/JWT/JWTComponent.java
@@ -1,5 +1,7 @@
 package br.com.zup.ZupNotion.config.security.JWT;
 
+import br.com.zup.ZupNotion.config.security.JWT.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,5 +24,14 @@ public class JWTComponent {
                 .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
 
         return token;
+    }
+
+    public Claims pegarClaims(String token){
+        try{
+            Claims claims = Jwts.parser().setSigningKey(segredo.getBytes()).parseClaimsJws(token).getBody();
+            return claims;
+        }catch (Exception e){
+            throw new TokenInvalidoException();
+        }
     }
 }
