6e281cb17f7e0b1fffa8781d4c83d07f4e901293
Savio <pauloluizsavio@gmail.com>
2022-01-25 17:14:52 -0300

    testar controller

---
+++ src/main/java/br/com/zup/ZupNotion/models/dtos/CadastroRetornoDTO.java
@@ -0,0 +1,11 @@
+package br.com.zup.ZupNotion.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class CadastroRetornoDTO {
+
+    private String nome;
+    private String email;
+
+}
--- src/main/java/br/com/zup/ZupNotion/models/dtos/CadastroUsuarioDTO.java
+++ src/main/java/br/com/zup/ZupNotion/models/dtos/CadastroUsuarioDTO.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
+
 public class CadastroUsuarioDTO {
 
     @NotBlank(message = "{validacao.not-blank}")
