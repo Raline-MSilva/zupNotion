ed7a1f6db63f265c787727e4f060b219262fee00
Raline Silva <ralinesilva1@gmail.com>
2022-01-25 14:05:58 -0300

    criar classe usuario logado

---
+++ src/main/java/br/com/zup/ZupNotion/config/security/JWT/UsuarioLogado.java
@@ -0,0 +1,53 @@
+package br.com.zup.ZupNotion.config.security.JWT;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+public class UsuarioLogado implements UserDetails {
+
+    private String id;
+    private String email;
+    private String senha;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
