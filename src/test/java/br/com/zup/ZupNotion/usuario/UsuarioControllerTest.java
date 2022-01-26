07ec1316768247329142281082f6e52c5f271262
Anna Karolyne <dev.akvc@gmail.com>
2022-01-26 11:23:20 -0300

    alterar post controller para void e testar

--- src/test/java/br/com/zup/ZupNotion/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/ZupNotion/usuario/UsuarioControllerTest.java
@@ -5,7 +5,7 @@ import br.com.zup.ZupNotion.config.security.JWT.JWTComponent;
 import br.com.zup.ZupNotion.config.security.JWT.UsuarioLoginService;
 import br.com.zup.ZupNotion.controllers.UsuarioController;
 import br.com.zup.ZupNotion.models.Usuario;
-import br.com.zup.ZupNotion.models.dtos.CadastroRetornoDTO;
+import br.com.zup.ZupNotion.models.dtos.AlterarSenhaDTO;
 import br.com.zup.ZupNotion.models.dtos.CadastroUsuarioDTO;
 import br.com.zup.ZupNotion.services.SenhaService;
 import br.com.zup.ZupNotion.services.UsuarioService;
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -46,7 +47,7 @@ public class UsuarioControllerTest {
     private ObjectMapper objectMapper;
     private Usuario usuario;
     private CadastroUsuarioDTO cadastroUsuarioDTO;
-    private CadastroRetornoDTO cadastroRetornoDTO;
+    private AlterarSenhaDTO alterarSenhaDTO;
 
     @BeforeEach
     public void setup() {
@@ -63,15 +64,15 @@ public class UsuarioControllerTest {
         cadastroUsuarioDTO.setEmail("fulano@zup.com.br");
         cadastroUsuarioDTO.setSenha("Fulano@12");
 
-        cadastroRetornoDTO = new CadastroRetornoDTO();
-        cadastroRetornoDTO.setNome("Fulano");
-        cadastroRetornoDTO.setEmail("fulano@zup.com.br");
+        alterarSenhaDTO = new AlterarSenhaDTO();
+        alterarSenhaDTO.setEmail("fulano@zup.com.br");
+        alterarSenhaDTO.setSenha("AC@432ab");
 
     }
 
     private ResultActions realizarRequisicao(Object object, int statusEsperado, String httpVerbo, String complemento) throws Exception {
         String json = objectMapper.writeValueAsString(object);
-        URI uri = UriComponentsBuilder.fromPath("/usuario"+complemento).build().toUri();
+        URI uri = UriComponentsBuilder.fromPath("/usuario" + complemento).build().toUri();
 
         return mockMvc.perform(MockMvcRequestBuilders.request(httpVerbo, uri)
                         .content(json).contentType(MediaType.APPLICATION_JSON))
