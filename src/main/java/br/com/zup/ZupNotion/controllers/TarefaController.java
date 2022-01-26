3c211588ad46d23dc9e90505088947acdba5cfc0
Raline Silva <ralinesilva1@gmail.com>
2022-01-26 15:38:08 -0300

    mapear endpoint tarefas

--- src/main/java/br/com/zup/ZupNotion/controllers/TarefaController.java
+++ src/main/java/br/com/zup/ZupNotion/controllers/TarefaController.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/tarefas")
 public class TarefaController {
     @Autowired
     private TarefaService tarefaService;
