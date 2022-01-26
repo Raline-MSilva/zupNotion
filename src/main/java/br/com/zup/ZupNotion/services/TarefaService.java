fcfa41f12a9cbb3b7c80adf2c831129fc4932c25
Elaine-dss <elaine.silva@zup.com.br>
2022-01-26 14:08:09 -0300

    criar service da tarefa

---
+++ src/main/java/br/com/zup/ZupNotion/services/TarefaService.java
@@ -0,0 +1,17 @@
+package br.com.zup.ZupNotion.services;
+
+import br.com.zup.ZupNotion.repositories.TarefaRepository;
+import br.com.zup.ZupNotion.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+}
