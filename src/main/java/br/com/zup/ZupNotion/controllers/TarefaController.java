3ba9feb2729da6f2bef227b77cde3508c4120dd2
Raline Silva <ralinesilva1@gmail.com>
2022-01-26 15:32:46 -0300

    modelar metodo cadastrar tarefa na controller

---
+++ src/main/java/br/com/zup/ZupNotion/controllers/TarefaController.java
@@ -0,0 +1,32 @@
+package br.com.zup.ZupNotion.controllers;
+
+import br.com.zup.ZupNotion.models.Tarefa;
+import br.com.zup.ZupNotion.models.dtos.CadastroTarefaDTO;
+import br.com.zup.ZupNotion.services.TarefaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class TarefaController {
+    @Autowired
+    private TarefaService tarefaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RespostaTarefaDTO cadastrar (@RequestBody @Valid CadastroTarefaDTO cadastroTarefaDTO) {
+        Tarefa tarefa = modelMapper.map(cadastroTarefaDTO, Tarefa.class);
+        return modelMapper.map(tarefaService.cadastrarTarefa(tarefa), RespostaTarefaDTO.class);
+    }
+
+}
