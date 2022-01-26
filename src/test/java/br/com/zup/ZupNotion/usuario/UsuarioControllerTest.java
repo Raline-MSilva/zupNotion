5367e15dc187955b8045ce64851cedcdda4a8e9a
Anna Karolyne <dev.akvc@gmail.com>
2022-01-25 22:05:21 -0300

    testar cadastro de usuario na controller

--- src/test/java/br/com/zup/ZupNotion/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/ZupNotion/usuario/UsuarioControllerTest.java
@@ -1,10 +1,13 @@
 package br.com.zup.ZupNotion.usuario;
 
+import br.com.zup.ZupNotion.components.Conversor;
 import br.com.zup.ZupNotion.config.security.JWT.JWTComponent;
 import br.com.zup.ZupNotion.config.security.JWT.UsuarioLoginService;
 import br.com.zup.ZupNotion.controllers.UsuarioController;
 import br.com.zup.ZupNotion.models.Usuario;
 import br.com.zup.ZupNotion.models.dtos.CadastroRetornoDTO;
+import br.com.zup.ZupNotion.models.dtos.CadastroUsuarioDTO;
+import br.com.zup.ZupNotion.services.SenhaService;
 import br.com.zup.ZupNotion.services.UsuarioService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,17 +17,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@WebMvcTest({UsuarioController.class, UsuarioLoginService.class, JWTComponent.class})
+import java.net.URI;
+
+@WebMvcTest({UsuarioController.class, Conversor.class, UsuarioLoginService.class, JWTComponent.class})
 public class UsuarioControllerTest {
 
     @MockBean
     private UsuarioService usuarioService;
+
+    @MockBean
+    private SenhaService senhaService;
 
     @MockBean
     private UsuarioLoginService usuarioLoginService;
@@ -37,34 +45,46 @@ public class UsuarioControllerTest {
 
     private ObjectMapper objectMapper;
     private Usuario usuario;
+    private CadastroUsuarioDTO cadastroUsuarioDTO;
     private CadastroRetornoDTO cadastroRetornoDTO;
 
     @BeforeEach
     public void setup() {
+        objectMapper = new ObjectMapper();
+
         usuario = new Usuario();
-        usuario.setNome("Xablau");
-        usuario.setEmail("xablau@zup.com.br");
+        usuario.setId("123abc");
+        usuario.setNome("Fulano");
+        usuario.setEmail("fulano@zup.com.br");
+        usuario.setSenha("CA@123we");
+
+        cadastroUsuarioDTO = new CadastroUsuarioDTO();
+        cadastroUsuarioDTO.setNome("Fulano");
+        cadastroUsuarioDTO.setEmail("fulano@zup.com.br");
+        cadastroUsuarioDTO.setSenha("Fulano@12");
 
         cadastroRetornoDTO = new CadastroRetornoDTO();
-        cadastroRetornoDTO.setNome("Xablau2");
-        cadastroRetornoDTO.setEmail("xablau2@zup.com");
+        cadastroRetornoDTO.setNome("Fulano");
+        cadastroRetornoDTO.setEmail("fulano@zup.com.br");
 
-        objectMapper = new ObjectMapper();
     }
 
+    private ResultActions realizarRequisicao(Object object, int statusEsperado, String httpVerbo, String complemento) throws Exception {
+        String json = objectMapper.writeValueAsString(object);
+        URI uri = UriComponentsBuilder.fromPath("/usuario"+complemento).build().toUri();
+
+        return mockMvc.perform(MockMvcRequestBuilders.request(httpVerbo, uri)
+                        .content(json).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(statusEsperado));
+    }
 
     @Test
-    @WithMockUser("user@user.com")
     public void testarCadastroUsuario() throws Exception {
         Mockito.when(usuarioService.cadastrarUsuario(Mockito.any(Usuario.class))).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(cadastroUsuarioDTO);
 
-        String json = objectMapper.writeValueAsString(cadastroRetornoDTO);
-
-        ResultActions resultado = mockMvc.perform(MockMvcRequestBuilders.post("/usuario")
-                        .content(json).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is(201));
-
-        String jsonResposta = resultado.andReturn().getResponse().getContentAsString();
-        var usuarioResposta = objectMapper.readValue(jsonResposta, CadastroRetornoDTO.class);
+        ResultActions resultadoEsperado = realizarRequisicao(usuario, 201, "POST", "");
+        String jsonResposta = resultadoEsperado.andReturn().getResponse().getContentAsString();
     }
+
 }
