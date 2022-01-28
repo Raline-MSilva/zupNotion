965895f439250ab9379ce34a8299f0b68bd1e79c
Elaine-dss <elaine.silva@zup.com.br>
2022-01-28 14:51:47 -0300

    adicionar verificaçao de senha forte

--- src/main/java/br/com/zup/ZupNotion/models/dtos/AlterarSenhaDTO.java
+++ src/main/java/br/com/zup/ZupNotion/models/dtos/AlterarSenhaDTO.java
@@ -3,6 +3,7 @@ package br.com.zup.ZupNotion.models.dtos;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class AlterarSenhaDTO {
@@ -10,6 +11,8 @@ public class AlterarSenhaDTO {
     @NotBlank(message = "{validacao.not-blank}")
     private String email;
     @NotBlank(message = "{validacao.not-blank}")
+    @Pattern(regexp ="^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$",
+            message="{validacao.senha-forte}")
     private String senha;
 
 }
