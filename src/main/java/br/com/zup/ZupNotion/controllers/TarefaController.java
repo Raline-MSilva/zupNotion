d738b1f74e1c8b59433cafb599e0a6bc2976ebbb
Anna Karolyne <dev.akvc@gmail.com>
2022-01-31 21:09:03 -0300

    configurar requisicao get com paginacao

--- src/main/java/br/com/zup/ZupNotion/controllers/TarefaController.java
+++ src/main/java/br/com/zup/ZupNotion/controllers/TarefaController.java
@@ -11,18 +11,10 @@ import br.com.zup.ZupNotion.services.TarefaService;
 import br.com.zup.ZupNotion.services.UsuarioLogadoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -47,16 +39,27 @@ public class TarefaController {
         return modelMapper.map(tarefaService.cadastrarTarefa(tarefa, usuarioLogadoService.pegarId()), RespostaTarefaDTO.class);
     }
 
-    @GetMapping
-    public List<TarefaResumoDTO> buscarTarefas(@RequestParam(required = false) Status status,
-                                               @RequestParam(required = false)Prioridade prioridade){
-        List<TarefaResumoDTO> tarefas = new ArrayList<>();
+//    @GetMapping
+//    public List<TarefaResumoDTO> buscarTarefas(@RequestParam(required = false) Status status,
+//                                               @RequestParam(required = false) Prioridade prioridade){
+//        List<TarefaResumoDTO> tarefas = new ArrayList<>();
+//
+//        for (Tarefa tarefa : tarefaService.buscarTarefas(usuarioLogadoService.pegarId(), status, prioridade)){
+//            TarefaResumoDTO tarefaResumoDTO = modelMapper.map(tarefa, TarefaResumoDTO.class);
+//            tarefas.add(tarefaResumoDTO);
+//        }
+//        return tarefas;
+//    }
 
-        for (Tarefa tarefa : tarefaService.buscarTarefas(usuarioLogadoService.pegarId(), status, prioridade)){
-            TarefaResumoDTO tarefaResumoDTO = modelMapper.map(tarefa, TarefaResumoDTO.class);
-            tarefas.add(tarefaResumoDTO);
-        }
-        return tarefas;
+    @GetMapping
+    public ResponseEntity<Page<TarefaResumoDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "3") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "titulo") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+        Page<Tarefa> list = tarefaService.findPage(page, linesPerPage, orderBy, direction);
+        Page<TarefaResumoDTO> listDto = list.map(TarefaResumoDTO::new);
+        return ResponseEntity.ok().body(listDto);
     }
 
     @PatchMapping("/{id}")
