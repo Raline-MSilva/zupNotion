db136c70592c7999bb501c5c1c1ef1704b96a601
Anna Karolyne <dev.akvc@gmail.com>
2022-02-01 14:29:24 -0300

    adicionar construtores

--- src/main/java/br/com/zup/ZupNotion/models/dtos/TarefaResumoDTO.java
+++ src/main/java/br/com/zup/ZupNotion/models/dtos/TarefaResumoDTO.java
@@ -1,11 +1,14 @@
 package br.com.zup.ZupNotion.models.dtos;
 
-import br.com.zup.ZupNotion.models.Tarefa;
 import br.com.zup.ZupNotion.models.enums.Prioridade;
 import br.com.zup.ZupNotion.models.enums.Status;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TarefaResumoDTO {
     private Integer id;
     private String titulo;
@@ -13,14 +16,5 @@ public class TarefaResumoDTO {
     private Prioridade prioridade;
     private Status status;
     private int estimativaEmHoras;
-
-    public TarefaResumoDTO(Tarefa obj) {
-        this.id = obj.getId();
-        this.titulo = obj.getTitulo();
-        this.descricao = obj.getDescricao();
-        this.prioridade = obj.getPrioridade();
-        this.status = obj.getStatus();
-        this.estimativaEmHoras = obj.getEstimativaEmHoras();
-    }
 
 }
