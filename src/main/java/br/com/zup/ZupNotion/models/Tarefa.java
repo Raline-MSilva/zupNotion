2c521fdcdd3d7b640ac197e2eebc765dc31ffe92
Anna Karolyne <dev.akvc@gmail.com>
2022-02-02 11:21:18 -0300

    criar construtor na model tarefa

--- src/main/java/br/com/zup/ZupNotion/models/Tarefa.java
+++ src/main/java/br/com/zup/ZupNotion/models/Tarefa.java
@@ -2,17 +2,19 @@ package br.com.zup.ZupNotion.models;
 
 import br.com.zup.ZupNotion.models.enums.Prioridade;
 import br.com.zup.ZupNotion.models.enums.Status;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tarefas")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tarefa {
 
     @Id
@@ -25,13 +27,20 @@ public class Tarefa {
     @Enumerated(EnumType.STRING)
     private Prioridade prioridade;
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private Status status = Status.A_FAZER;
     @Column(name = "data_de_cadastro")
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime dataDeCadastro;
+    private LocalDateTime dataDeCadastro = LocalDateTime.now();
     @Column(name = "estimativa_em_horas")
     private int estimativaEmHoras;
     @ManyToOne
     private Usuario usuario;
+
+    public Tarefa(String titulo, String descricao, Prioridade prioridade, int estimativaEmHoras) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.estimativaEmHoras = estimativaEmHoras;
+    }
 
 }
