edef1e2903ad50c6403198c81754a0b7c3a5486e
Anna Karolyne <dev.akvc@gmail.com>
2022-02-02 22:48:46 -0300

    fix metodo para receber arquivos .csv

--- src/main/java/br/com/zup/ZupNotion/services/TarefaService.java
+++ src/main/java/br/com/zup/ZupNotion/services/TarefaService.java
@@ -2,7 +2,7 @@ package br.com.zup.ZupNotion.services;
 
 import br.com.zup.ZupNotion.exceptions.TarefaNaoExisteException;
 import br.com.zup.ZupNotion.models.Tarefa;
-import br.com.zup.ZupNotion.models.TarefaImportacaoArquivo;
+import br.com.zup.ZupNotion.models.TarefaImportacaoCSV;
 import br.com.zup.ZupNotion.models.Usuario;
 import br.com.zup.ZupNotion.models.enums.Prioridade;
 import br.com.zup.ZupNotion.models.enums.Status;
@@ -13,7 +13,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +31,7 @@ public class TarefaService {
     private UsuarioRepository usuarioRepository;
 
     @Autowired
-    private TarefaImportacaoArquivo importacaoArquivo;
+    private TarefaImportacaoCSV importacaoCSV;
 
     public Tarefa cadastrarTarefa(Tarefa tarefa, String id) {
         Optional<Usuario> usuario = usuarioRepository.findById(id);
@@ -93,8 +95,20 @@ public class TarefaService {
         }
     }
 
-    public void importarArquivosCSV (String caminho, String usuarioId){
-        importacaoArquivo.salvarTarefasComUsuario(caminho, usuarioId);
+    public void salvarCSV(MultipartFile file, String usuarioId) {
+        try {
+            List<Tarefa> listaTarefas = importacaoCSV.salvarTarefasComUsuario(usuarioId, file.getInputStream());
+            tarefaRepository.saveAll(listaTarefas);
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+    }
+
+    public boolean validarSeArquivoCSV(MultipartFile file){
+        if(importacaoCSV.verificarFormatoCSV(file)){
+            return true;
+        }
+        return false;
     }
 
 }
