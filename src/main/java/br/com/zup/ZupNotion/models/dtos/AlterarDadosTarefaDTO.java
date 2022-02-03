555a4971c1f5514d6daf0d89e5a8a2c67e227c38
Anna Karolyne <dev.akvc@gmail.com>
2022-02-03 15:31:04 -0300

    criar dto para alterar dados tarefa

--- src/main/java/br/com/zup/ZupNotion/models/dtos/AlterarDadosTarefaDTO.java
+++ src/main/java/br/com/zup/ZupNotion/models/dtos/AlterarDadosTarefaDTO.java
@@ -3,12 +3,13 @@ package br.com.zup.ZupNotion.models.dtos;
 import br.com.zup.ZupNotion.models.enums.Status;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
-public class AlterarStatusDTO {
-
-    @NotNull
+public class AlterarDadosTarefaDTO {
     private Status status;
+    private String titulo;
+    @Size(min = 3, max = 100, message = "{validacao.descricao-size}")
+    private String descricao;
 
 }
