29346f6c4da0b30f89381b3eda97c6ff137a12bc
Raline Silva <ralinesilva1@gmail.com>
2022-02-08 15:55:02 -0300

    alterar testes

--- src/test/java/br/com/zup/ZupNotion/services/TarefaServiceTest.java
+++ src/test/java/br/com/zup/ZupNotion/services/TarefaServiceTest.java
@@ -2,6 +2,7 @@ package br.com.zup.ZupNotion.services;
 
 import br.com.zup.ZupNotion.exceptions.TarefaNaoExisteException;
 import br.com.zup.ZupNotion.models.Tarefa;
+import br.com.zup.ZupNotion.models.Usuario;
 import br.com.zup.ZupNotion.models.enums.Prioridade;
 import br.com.zup.ZupNotion.repositories.TarefaRepository;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -20,33 +23,51 @@ public class TarefaServiceTest {
     @MockBean
     private TarefaRepository tarefaRepository;
 
+    @MockBean
+    private UsuarioService usuarioService;
+
     @Autowired
     private TarefaService tarefaService;
 
     private Tarefa tarefa;
+    private Usuario usuario;
+    private Usuario usuario2;
 
     @BeforeEach
     public void setup(){
         tarefa = new Tarefa();
+        tarefa.setId(1);
         tarefa.setTitulo("Testar");
         tarefa.setDescricao("Testar deletar");
         tarefa.setPrioridade(Prioridade.ALTA);
+
+        usuario = new Usuario();
+        usuario.setNome("ana");
+        tarefa.setUsuario(usuario);
+
+        List<Tarefa> tarefas = new ArrayList<>();
+        usuario.setTarefas(tarefas);
+        usuario.getTarefas().add(tarefa);
+
+        usuario2 = new Usuario();
+        usuario2.setNome("lia");
     }
 
     @Test
     public void testarDeletarTarefaSucesso(){
+        Mockito.when(usuarioService.buscarUsuarioLogado()).thenReturn(usuario);
         Mockito.when(tarefaRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(tarefa));
         Mockito.doNothing().when(tarefaRepository).deleteById(Mockito.anyInt());
 
-        tarefaService.deletarTarefa(Mockito.anyInt());
-
+        tarefaService.deletarTarefa(tarefa.getId());
         Mockito.verify(tarefaRepository, Mockito.times(1)).deleteById(Mockito.anyInt());
 
     }
 
     @Test
     public void testarDeletarTarefaCaminhoNegativo(){
-        Mockito.when(tarefaRepository.existsById(Mockito.anyInt())).thenReturn(false);
+        Mockito.when(usuarioService.buscarUsuarioLogado()).thenReturn(usuario2);
+        Mockito.when(tarefaRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(tarefa));
         Mockito.doNothing().when(tarefaRepository).deleteById(Mockito.anyInt());
 
         TarefaNaoExisteException exception = Assertions.assertThrows(TarefaNaoExisteException.class, () -> {
@@ -55,7 +76,7 @@ public class TarefaServiceTest {
     }
 
     @Test
-    public void testarBuscarTarefa(){
+    public void testarLocalizarTarefaPorId(){
         Mockito.when(tarefaRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(tarefa));
         Tarefa tarefaResposta = tarefaService.localizarTarefaPorId(Mockito.anyInt());
 
@@ -67,7 +88,7 @@ public class TarefaServiceTest {
     }
 
     @Test
-    public void testarBuscarTarefaNaoEncontrado(){
+    public void testarLocalizarTarefaPorIdNaoEncontrado(){
         Mockito.when(tarefaRepository.save(Mockito.any())).thenReturn(tarefa);
         Mockito.when(tarefaRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
 
